fab5b6d180476366fed3726473f67b155f422e66
JavanAppleby <styx_n_stonz@hotmail.com>
2022-01-09 18:03:53 +1000

    restructuring

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-
+// Here we render the App component, passing the Marvel API key as property.
+// Since our application does not have access to environment variables at
+// runtime, the API key must be read in and exposed from within this file.
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App apiKey={process.env.REACT_APP_MARVEL_API_KEY} />,
+  document.getElementById("root")
 );
---
+++ src/services/MarvelService.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+export class MarvelService {
+  // --------------------------------------------------
+  // ENDPOINTS
+  // --------------------------------------------------
+  static get ENDPOINTS() {
+    return {
+      comic: "https://gateway.marvel.com:443/v1/public/comics",
+      comics: "https://gateway.marvel.com:443/v1/public/comics",
+      character: "https://gateway.marvel.com:443/v1/public/characters",
+      characters: "https://gateway.marvel.com:443/v1/public/characters",
+    };
+  }
+
+  // --------------------------------------------------
+  // SETUP
+  // --------------------------------------------------
+  constructor(config) {
+    this.apiKey = config.apiKey;
+  }
+
+  // --------------------------------------------------
+  // AUTHENTICATION
+  // --------------------------------------------------
+  getAuthConfig() {
+    return { apikey: this.apiKey };
+  }
+
+  // --------------------------------------------------
+  // CHARACTERS-RELATED METHODS
+  // --------------------------------------------------
+  getCharacters(config = {}) {
+    console.warn(
+      "Whoops, it looks like this method hasn't been implemented yet."
+    );
+    // TODO:
+    // - Create the `params` object.
+    // - Extract the correct endpoint from `ENDPOINTS`.
+    // - Dispatch a request using `axios.get()`.
+    // - Parse and return the response.
+    const params = { ...config, ...this.getAuthConfig() };
+    const endpoint = MarvelService.ENDPOINTS.characters;
+    return axios.get(endpoint, { params: params }).then((response) => {
+      return response.data.data;
+    });
+  }
+
+  getCharacter(id, config = {}) {
+    console.warn(
+      "Whoops, it looks like this method hasn't been implemented yet."
+    );
+    // TODO:
+    // - Create the `params` object.
+    // - Extract the correct endpoint from `ENDPOINTS`; add the `id`.
+    // - Dispatch a request using `axios.get()`.
+    // - Parse and return the response.
+    const params = { ...config, ...this.getAuthConfig() };
+    const endpoint = MarvelService.ENDPOINTS.character + "/" + id;
+    return axios.get(endpoint, { params: params }).then((response) => {
+      return response.data.data;
+    });
+  }
+}
