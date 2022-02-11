644293cae6062deab408a0381e5ce49be2d47450
JavanAppleby <styx_n_stonz@hotmail.com>
2022-02-11 19:01:47 +1000

    added username input (not working)

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,20 +2,20 @@ import React from "react";
 import "../App.css";
 
 function About() {
-  //   function handleSubmit() {
-  //     let username = sessionStorage.getItem("Username") || "";
-  //     console.log(username);
-  //     username.push(uname);
-  //   }
+  function handleSubmit(uname) {
+    let username = sessionStorage.getItem("Username") || "";
+    username.push(uname.value);
+    sessionStorage.setItem("Username");
+  }
+  const uname = document.getElementById("uname");
 
   return (
     <div>
       <h1>About</h1>
       <br></br>
-      <form>
-        {/* onSubmit={handleSubmit}> */}
-        <label for="uname">Username:</label>
-        <input type="text" id="uname" name="uname" required="true" />
+      <form onSubmit={() => handleSubmit(uname)}>
+        <label htmlFor="uname">Username:</label>
+        <input type="text" id="uname" name="uname" required={true} />
         <br></br>
         <input type="submit" value="Submit" />
       </form>
